a9db333da6b95ab27861ab51ea63667a3654f7ab
fiQoGithub <mohamed.shafiq@gmail.com>
2024-10-28 21:19:34 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 
+  backend "s3" {
+    bucket = "shafiq-s3-terraform-3.9"
+    key    = "terraform.tfstate"
+    region = "us-east-1" # Replace with your desired region
+
+    # Optional: Enable encryption at rest
+    dynamodb_table = "your-dynamodb-table-name"
+    encrypt         = true
+  }
+}
